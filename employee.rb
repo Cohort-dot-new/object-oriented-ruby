840a9420b4e00f9616f6e46de62f4528f7cdcbb3
Peter Jang <peter.x.jang@gmail.com>
2016-10-31 20:47:13 -0500

    Rewrite initialize to use single options hash

--- employee.rb
+++ employee.rb
@@ -1,30 +1,30 @@
-# Arrays
-employee1 = ["Majora", "Carter", 80000, true]
-employee2 = ["Danilo", "Campos", 70000, true]
-puts employee1[0] + " " + employee1[1] + " makes " + employee1[2].to_s + " a year."
-puts "#{employee2[0]} #{employee2[1]} makes #{employee2[2]} a year."
+# # Arrays
+# employee1 = ["Majora", "Carter", 80000, true]
+# employee2 = ["Danilo", "Campos", 70000, true]
+# puts employee1[0] + " " + employee1[1] + " makes " + employee1[2].to_s + " a year."
+# puts "#{employee2[0]} #{employee2[1]} makes #{employee2[2]} a year."
 
-# Hashes
-employee1 = {"first_name" => "Majora", "last_name" => "Carter", "salary" => 80000, "active" => true}
-employee2 = {"first_name" => "Danilo", "last_name" => "Campos", "salary" => 70000, "active" => true}
-puts "#{employee1['first_name']} #{employee1['last_name']} makes #{employee1['salary']} a year."
-puts "#{employee2['first_name']} #{employee2['last_name']} makes #{employee2['salary']} a year."
+# # Hashes
+# employee1 = {"first_name" => "Majora", "last_name" => "Carter", "salary" => 80000, "active" => true}
+# employee2 = {"first_name" => "Danilo", "last_name" => "Campos", "salary" => 70000, "active" => true}
+# puts "#{employee1['first_name']} #{employee1['last_name']} makes #{employee1['salary']} a year."
+# puts "#{employee2['first_name']} #{employee2['last_name']} makes #{employee2['salary']} a year."
 
-# Hashes using symbols for keys
-employee1 = {:first_name => "Majora", :last_name => "Carter", :salary => 80000, :active => true}
-employee2 = {first_name: "Danilo", last_name: "Campos", salary: 70000, active: true}
-puts "#{employee1[:first_name]} #{employee1[:last_name]} makes #{employee1[:salary]} a year."
-puts "#{employee2[:first_name]} #{employee2[:last_name]} makes #{employee2[:salary]} a year."
+# # Hashes using symbols for keys
+# employee1 = {:first_name => "Majora", :last_name => "Carter", :salary => 80000, :active => true}
+# employee2 = {first_name: "Danilo", last_name: "Campos", salary: 70000, active: true}
+# puts "#{employee1[:first_name]} #{employee1[:last_name]} makes #{employee1[:salary]} a year."
+# puts "#{employee2[:first_name]} #{employee2[:last_name]} makes #{employee2[:salary]} a year."
 
 # Instances of a class
 class Employee
   attr_reader :first_name, :last_name
 
-  def initialize(input_first_name, input_last_name, input_salary, input_status)
-    @first_name = input_first_name
-    @last_name = input_last_name
-    @salary = input_salary
-    @status = input_status
+  def initialize(input_options)
+    @first_name = input_options[:first_name]
+    @last_name = input_options[:last_name]
+    @salary = input_options[:salary]
+    @status = input_options[:status]
   end
 
   def print_info
@@ -36,8 +36,8 @@ class Employee
   end
 end
 
-employee1 = Employee.new("Majora", "Carter", 80000, true)
-employee2 = Employee.new("Danilo", "Campos", 70000, true)
+employee1 = Employee.new({first_name: "Majora", last_name: "Carter", salary: 80000, active: true})
+employee2 = Employee.new(first_name: "Danilo", last_name: "Campos", salary: 70000, active: true)
 employee1.print_info
 employee2.print_info
 employee2.give_annual_raise
