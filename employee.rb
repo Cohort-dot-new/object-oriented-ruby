19031cd9e1c59bf0d74d25cb1e259de7f0f8f49d
Peter Jang <peter.x.jang@gmail.com>
2016-10-31 20:21:24 -0500

    Model employees using class instances

--- employee.rb
+++ employee.rb
@@ -15,3 +15,38 @@ employee1 = {:first_name => "Majora", :last_name => "Carter", :salary => 80000, 
 employee2 = {first_name: "Danilo", last_name: "Campos", salary: 70000, active: true}
 puts "#{employee1[:first_name]} #{employee1[:last_name]} makes #{employee1[:salary]} a year."
 puts "#{employee2[:first_name]} #{employee2[:last_name]} makes #{employee2[:salary]} a year."
+
+# Instances of a class
+class Employee
+  def initialize(input_first_name, input_last_name, input_salary, input_status)
+    @first_name = input_first_name
+    @last_name = input_last_name
+    @salary = input_salary
+    @status = input_status
+  end
+
+  def first_name
+    @first_name
+  end
+
+  def last_name
+    @last_name
+  end
+
+  def print_info
+    puts "#{@first_name} #{@last_name} makes #{@salary} a year."
+  end
+
+  def give_annual_raise
+    @salary = 1.05 * @salary
+  end
+end
+
+employee1 = Employee.new("Majora", "Carter", 80000, true)
+employee2 = Employee.new("Danilo", "Campos", 70000, true)
+employee1.print_info
+employee2.print_info
+employee2.give_annual_raise
+employee2.print_info
+
+puts employee1.first_name
