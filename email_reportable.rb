b70db938cf915b46fad2f4d9f486cf655ee77098
Peter Jang <peter.x.jang@gmail.com>
2016-11-02 18:58:24 -0500

    Refactor classes and modules to separate files

---
+++ email_reportable.rb
@@ -0,0 +1,13 @@
+module EmailReportable
+  def send_report
+    puts "Sending email..."
+    # use email sending library...
+    puts "Email sent!"
+  end
+
+  def receive_report
+    puts "Receiving email..."
+    # write code
+    puts "Email received!"
+  end
+end
--- employee.rb
+++ employee.rb
@@ -1,30 +1,12 @@
-# # Arrays
-# employee1 = ["Majora", "Carter", 80000, true]
-# employee2 = ["Danilo", "Campos", 70000, true]
-# puts employee1[0] + " " + employee1[1] + " makes " + employee1[2].to_s + " a year."
-# puts "#{employee2[0]} #{employee2[1]} makes #{employee2[2]} a year."
-
-# # Hashes
-# employee1 = {"first_name" => "Majora", "last_name" => "Carter", "salary" => 80000, "active" => true}
-# employee2 = {"first_name" => "Danilo", "last_name" => "Campos", "salary" => 70000, "active" => true}
-# puts "#{employee1['first_name']} #{employee1['last_name']} makes #{employee1['salary']} a year."
-# puts "#{employee2['first_name']} #{employee2['last_name']} makes #{employee2['salary']} a year."
-
-# # Hashes using symbols for keys
-# employee1 = {:first_name => "Majora", :last_name => "Carter", :salary => 80000, :active => true}
-# employee2 = {first_name: "Danilo", last_name: "Campos", salary: 70000, active: true}
-# puts "#{employee1[:first_name]} #{employee1[:last_name]} makes #{employee1[:salary]} a year."
-# puts "#{employee2[:first_name]} #{employee2[:last_name]} makes #{employee2[:salary]} a year."
-
-# Instances of a class
 class Employee
   attr_reader :first_name, :last_name
+  attr_accessor :active
 
   def initialize(input_options)
     @first_name = input_options[:first_name]
     @last_name = input_options[:last_name]
     @salary = input_options[:salary]
-    @status = input_options[:status]
+    @active = input_options[:active]
   end
 
   def print_info
@@ -35,12 +17,3 @@ class Employee
     @salary = 1.05 * @salary
   end
 end
-
-employee1 = Employee.new({first_name: "Majora", last_name: "Carter", salary: 80000, active: true})
-employee2 = Employee.new(first_name: "Danilo", last_name: "Campos", salary: 70000, active: true)
-employee1.print_info
-employee2.print_info
-employee2.give_annual_raise
-employee2.print_info
-
-puts employee1.first_name
